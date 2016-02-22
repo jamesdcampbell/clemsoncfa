b46a87f975722cd2ef42b372a947d8550bc39c5c
Sheldon Juncker <sheldonjuncker@gmail.com>
2016-02-22 14:42:45 -0500

    fixing broken merge.

--- performance/admin/index.php
+++ performance/admin/index.php
@@ -1,65 +1,7 @@
-<<<<<<< HEAD
-<?php
-=======
-<!DOCTYPE html>
 <?php
 include '../includes/init.php';
 include '../includes/header.php';
-include '../includes/footer.php';
 ?>
-<?php /*<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../favicon.ico">
->>>>>>> 01bd73f8314e4b66574796a4f9040824d7bfb029
-
-include '../includes/init.php';
-include '../includes/header.php';
-
-<<<<<<< HEAD
-?>
-=======
-    <!-- Bootstrap core CSS -->
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="../bootstrap/css/dashboard.css" rel="stylesheet">
-  </head>
-  */?>
-
-  <body>
->>>>>>> 01bd73f8314e4b66574796a4f9040824d7bfb029
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Performance Review System</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Help</a></li>
-          </ul>
-          <form class="navbar-form navbar-right">
-            <input type="text" class="form-control" placeholder="Search...">
-          </form>
-        </div>
-      </div>
-    </nav>
-
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
