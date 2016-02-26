b835e5a27516274c31f6c625e3a9a1cff86835d1
Daniel Cochran <Daniel Cochran>
2016-02-25 22:47:51 -0500

    Some minor header elements fixed. Might need more improvement.

--- performance/manager/review.php
+++ performance/manager/review.php
@@ -36,7 +36,7 @@ include '../includes/header.php';
 <div class="col-sm-3 col-md-2 sidebar">
 		 <img src="../../images/cfaicon.png" class="img-responsive" alt="../../images/cfaicon.jpg" width="304" height="236"> 
 			<h1 class="welcome">Welcome back!</h1>
-			<h1 class="manager_name">$Manager Name</h1>
+			<h1 class="manager_name">Manager Name</h1>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Review Employee</h1>
