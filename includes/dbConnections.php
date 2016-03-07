87c87e83ae08878521a91307119d433d1c5f84af
Sheldon Juncker <sheldonjuncker@gmail.com>
2016-03-07 15:30:15 -0500

    Testing .gitignore.

--- includes/dbConnections.php
+++ includes/dbConnections.php
@@ -1,4 +1,4 @@
-<?php
+<?php blah blah
 	/* File: dbConnections.php
 	 * Purpose: just has the db connection string	 
 	 * Misc: If you include this file in other files that you interact with the databse with
